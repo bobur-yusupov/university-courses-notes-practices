046ce3bc30d03f2b5ca9e41238fdbcd8e03130fc
Bobur Yusupov <bobur.yu06@mail.ru>
2025-02-13 17:17:03 +0100

    Person class in Java

---
+++ oop1/lab1/Person/src/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+import com.example.person.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person p1 = new Person("Bobur Yusupov", 32, "male");
+
+        p1.introduce();
+    }
+}
--- oop1/lab1/Person/src/com/example/person/Person.java
+++ oop1/lab1/Person/src/com/example/person/Person.java
@@ -1,4 +1,21 @@
 package com.example.person;
 
 public class Person {
+    private String name;
+    private int age;
+    private String gender;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public void introduce() {
+        System.out.println("My name is " + this.name + ". I am " + this.age + " old. I am a " + this.gender);
+    }
+
+    public void birthday() {
+        this.age++;
+    }
 }
