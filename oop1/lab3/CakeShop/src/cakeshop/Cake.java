4660f294970e07f393670df33f672ba910fd7849
Yusupov Boburjon <ytajdi@INF.ELTE.HU>
2025-02-26 15:01:01 +0100

    CakeShop project

---
+++ oop1/lab3/CakeShop/src/cakeshop/Cake.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cakeshop;
+
+/**
+ *
+ * @author ytajdi
+ */
+public class Cake {
+    public String name;
+    public String color;
+    public double weight;
+    public double cost;
+}
---
+++ oop1/lab3/CakeShop/src/cakeshop/CakeShop.java
@@ -0,0 +1,45 @@
+package cakeshop;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class CakeShop {
+    private List<Cake> cakes = new ArrayList<>();
+    private int totalSold = 0;
+    
+    public int countByColor(String color) {
+        int totalCakes = 0;
+        for (Cake cake : cakes) {
+            if (cake.color.equals(color)) {
+                totalCakes++;
+            }
+        }
+        
+        return totalCakes;
+    }
+    
+    public double totalWorth() {
+        double totalWorth = 0.0;
+        
+        for (Cake cake : cakes) {
+            totalWorth += cake.cost;
+        }
+        
+        return totalWorth;
+    }
+    
+    public void registerNewCake(Cake newCake) {
+        cakes.add(newCake);
+    }
+    
+    public void sellCake(int index) {
+        cakes.remove(index);
+        
+        totalSold ++;
+    }
+    
+    public int getTotalSold() {
+        return this.totalSold;
+    }
+    
+}
