497d5c8c571f118a2fc912ac2364e5da4ca9f6b6
Bobur Yusupov <bobur.yu06@mail.ru>
2025-04-09 13:06:07 +0200

    WEBDEV: added navbar responsivity

--- webdev/ziyadatbank/js/navbar.js
+++ webdev/ziyadatbank/js/navbar.js
@@ -5,10 +5,12 @@ const closeBtn = document.querySelector("#closeNavbarMobile");
 
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("visible");
+    navMenu.classList.toggle("open");
     navMenu.classList.toggle("hidden");
     console.log("Hamburger clicked");
 });
 closeBtn.addEventListener("click", () => {
     navMenu.classList.toggle("visible");
+    navMenu.classList.remove("open");
     navMenu.classList.toggle("hidden");
 });
