ab06cf37d639e530c841072d393d0a9be0cb17df
Bobur Yusupov <bobur.yu06@mail.ru>
2025-04-18 21:08:17 +0200

    OOP: Relocated input files

--- oop1/assignments/big_assignment/WildlifeSimulation/src/wildlifesimulation/Simulation.java
+++ oop1/assignments/big_assignment/WildlifeSimulation/src/wildlifesimulation/Simulation.java
@@ -2,11 +2,22 @@ package wildlifesimulation;
 
 import wildlifesimulation.model.Animal;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Simulation {
     public static void main(String[] args) {
-        Animal lion = new Animal("Lion");
+        // Read animal, ranger, habitat and vehicle data from separate files
 
-        System.out.println(lion.getSpecies());
+        try {
+            File animalFile = new File("./animals.txt");
+            BufferedReader animalReader = new BufferedReader(new FileReader(animalFile.getAbsolutePath()));
+            System.out.println(animalReader.lines());
+        } catch (FileNotFoundException e) {
+            System.out.println("Error");
+        }
     }
     
 }
